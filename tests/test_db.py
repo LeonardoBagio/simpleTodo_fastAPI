8969783f172759dba73fa5d128ff7996a33a9ee9
Leonardo Bagio <leobagio2009@gmail.com>
2026-07-29 18:10:07 -0300

    aula 4

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,17 +1,26 @@
+from dataclasses import asdict
+from datetime import datetime
+
 from sqlalchemy import select
 
 from simple_todo.models import User
 
 
-def test_create_user(session):
-    new_user = User(username='teste', email='teste@teste', password='secret')
+def test_create_user(session, mock_db_time):
+    with mock_db_time(model=User, time=datetime.now()) as time:
+        new_user = User(
+            username='teste', email='teste@teste', password='secret'
+        )
 
-    session.add(new_user)
-    session.commit()
+        session.add(new_user)
+        session.commit()
 
-    user = session.scalar(select(User).where(User.username == 'test'))
+        user = session.scalar(select(User).where(User.username == 'teste'))
 
-    # assert user.id == 1
-    assert user.username == new_user.username
-    assert user.email == new_user.email
-    assert user.password == new_user.password
+    assert asdict(user) == {
+        'id': new_user.id,
+        'username': new_user.username,
+        'email': new_user.email,
+        'password': new_user.password,
+        'created_at': time,
+    }
