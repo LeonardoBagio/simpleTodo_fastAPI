28393846c9a037edc5d53cf587d36795d506acf8
Leonardo Bagio <leobagio2009@gmail.com>
2026-08-27 18:30:14 -0300

    Aula 11

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,13 +8,21 @@ import pytest_asyncio
 from fastapi.testclient import TestClient
 from sqlalchemy import event
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.pool import StaticPool
+from testcontainers.postgres import PostgresContainer
 
 from simple_todo.app import app
 from simple_todo.database import get_session
 from simple_todo.models import Todo, TodoState, User, table_registry
 from simple_todo.security import get_password_hash
 from simple_todo.settings import Settings
+
+
+@pytest.fixture(scope='session')
+def engine():
+    with PostgresContainer('postgres:16', driver='psycopg') as postgres:
+        _engine = create_async_engine(postgres.get_connection_url())
+
+        yield _engine
 
 
 @pytest.fixture
@@ -30,13 +38,7 @@ def client(session):
 
 
 @pytest_asyncio.fixture
-async def session():
-    engine = create_async_engine(
-        'sqlite+aiosqlite:///:memory:',
-        connect_args={'check_same_thread': False},
-        poolclass=StaticPool,
-    )
-
+async def session(engine):
     async with engine.begin() as conn:
         await conn.run_sync(table_registry.metadata.create_all)
 
